db5001fba8f69771bdf10ef1d064bf776b41fd4e
gollymashed <gm@wassie.games>
2023-07-24 23:13:28 +0100

    everythings working

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
-chrome.action.onClicked.addListener((tab) => {
-    if (tab.id) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['content.js']
-        });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log("Tab updated.");
+
+  if (tab.url) {
+    console.log("URL changed to " + tab.url + ".");
+    if (tab.url.includes("twitter.com")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["content.js"],
+      });
     }
+  }
 });
