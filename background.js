4d8307fb06919fc05413dfcc2476f477227c79b0
gollymashed <gm@wassie.games>
2023-07-24 23:46:55 +0100

    reduced permissions

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("Tab updated.");
-
   if (tab.url) {
-    console.log("URL changed to " + tab.url + ".");
     if (tab.url.includes("twitter.com")) {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
