293184b4caee641abc4594cca0515954d015740b
gollymashed <gm@wassie.games>
2023-07-28 15:27:40 +0100

    styling revamped and ui working

--- src/content.js
+++ src/content.js
@@ -1,55 +1,109 @@
-import { TwitterLogo, WassieLogo } from "../components/logos";
+import { TwitterLogo, WassieLogo } from '../components/logos';
+
+let isEnabled;
+let selectedLogo;
+let originalLogo = null;
 
 function generateLogo(colour) {
-  return WassieLogo(colour);
+    if (selectedLogo === 'twitter') {
+        return TwitterLogo(getLogoColor());
+    } else if (selectedLogo === 'wassie') {
+        return WassieLogo(getLogoColor());
+    } else {
+        return originalLogo;
+    }
 }
 
 function getLogoColor() {
-  const computedStyle = window.getComputedStyle(document.body);
-  const backgroundColor = computedStyle.backgroundColor;
+    const computedStyle = window.getComputedStyle(document.body);
+    const backgroundColor = computedStyle.backgroundColor;
 
-  if (backgroundColor === "rgb(255, 255, 255)") {
-    return "#242e36";
-  } else if (backgroundColor === "rgb(0, 0, 0)") {
-    return "#d6d9db";
-  } else if (backgroundColor === "rgb(21, 32, 43)") {
-    return "#d6d9db";
-  }
-  return "#242e36";
+    if (backgroundColor === 'rgb(255, 255, 255)') {
+        return '#242e36';
+    } else if (backgroundColor === 'rgb(0, 0, 0)') {
+        return '#d6d9db';
+    } else if (backgroundColor === 'rgb(21, 32, 43)') {
+        return '#d6d9db';
+    }
+    return '#242e36';
+}
+
+function setLogo() {
+    if (!originalLogo) {
+        const homeLogoLink = document.querySelector('a[href="/home"]');
+        if (homeLogoLink) {
+            originalLogo = homeLogoLink.querySelector('svg').outerHTML;
+        }
+    }
+    if (isEnabled) {
+        changeLoadingLogo();
+        changeHomeLogo();
+        changeFavicon();
+    } else {
+        restoreOriginalLogo();
+    }
 }
 
 function changeHomeLogo() {
-  const homeLogoLink = document.querySelector('a[href="/home"]');
-  if (homeLogoLink) {
-    const newLogo = homeLogoLink.querySelector("svg").cloneNode(true);
-    const newLogoContent = generateLogo(getLogoColor());
-    newLogo.innerHTML = newLogoContent;
-    homeLogoLink.querySelector("svg").outerHTML = newLogo.outerHTML;
-  }
+    const homeLogoLink = document.querySelector('a[href="/home"]');
+    if (homeLogoLink) {
+        const newLogo = homeLogoLink.querySelector('svg').cloneNode(true);
+        const newLogoContent = generateLogo(getLogoColor());
+        newLogo.innerHTML = newLogoContent;
+        homeLogoLink.querySelector('svg').outerHTML = newLogo.outerHTML;
+    }
 }
 
 function changeLoadingLogo() {
-  const loadingLogo = document.querySelector("#placeholder svg");
-  if (loadingLogo) {
-    const newLogo = loadingLogo.cloneNode(true);
-    const newLogoContent = generateLogo(getLogoColor());
-    newLogo.innerHTML = newLogoContent;
-    loadingLogo.outerHTML = newLogo.outerHTML;
-  }
+    const loadingLogo = document.querySelector('#placeholder svg');
+    if (loadingLogo) {
+        const newLogo = loadingLogo.cloneNode(true);
+        const newLogoContent = generateLogo(getLogoColor());
+        newLogo.innerHTML = newLogoContent;
+        loadingLogo.outerHTML = newLogo.outerHTML;
+    }
 }
 
 function changeFavicon() {
-  const faviconLink = document.querySelector('link[rel="shortcut icon"]');
-  if (faviconLink) {
-    faviconLink.href = "https://abs.twimg.com/favicons/twitter.ico";
-  }
+    const faviconLink = document.querySelector('link[rel="shortcut icon"]');
+    if (faviconLink) {
+        faviconLink.href = 'https://abs.twimg.com/favicons/twitter.ico';
+    }
 }
 
-function activateLogoChange() {
-  console.log("de-x activated.");
-  changeLoadingLogo();
-  changeHomeLogo();
-  changeFavicon();
+function restoreOriginalLogo() {
+    const homeLogoLink = document.querySelector('a[href="/home"]');
+    if (homeLogoLink) {
+        homeLogoLink.querySelector('svg').outerHTML = originalLogo;
+    }
+    const loadingLogo = document.querySelector('#placeholder svg');
+    if (loadingLogo) {
+        loadingLogo.outerHTML = originalLogo;
+    }
+    const faviconLink = document.querySelector('link[rel="shortcut icon"]');
+    if (faviconLink) {
+        faviconLink.href = 'https://abs.twimg.com/favicons/twitter.ico';
+    }
 }
 
-activateLogoChange();
+// Load the state from storage
+chrome.storage.sync.get(['enabled', 'selectedLogo'], function (result) {
+    isEnabled = result.hasOwnProperty('enabled') ? result.enabled : true;
+    selectedLogo = result.hasOwnProperty('selectedLogo') ? result.selectedLogo : 'twitter';
+    setLogo();
+});
+
+// Listen for changes in storage
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let key in changes) {
+        if (key === 'enabled' || key === 'selectedLogo') {
+            let storageChange = changes[key];
+            if (key === 'enabled') {
+                isEnabled = storageChange.newValue;
+            } else if (key === 'selectedLogo') {
+                selectedLogo = storageChange.newValue;
+            }
+            setLogo();
+        }
+    }
+});
--- src/popup.js
+++ src/popup.js
@@ -1,28 +1,53 @@
-import { TwitterLogo, WassieLogo } from "../components/logos.js";
+import { TwitterLogo, WassieLogo } from '../components/logos.js';
 
 // Set the SVGs as the innerHTML of the wrappers
-document.getElementById("twitter-logo-content").innerHTML = TwitterLogo("#000");
-document.getElementById("wassie-logo-content").innerHTML = WassieLogo("#000");
+document.getElementById('twitter-logo-content').innerHTML = TwitterLogo('#fff');
+document.getElementById('wassie-logo-content').innerHTML = WassieLogo('#fff');
+
+const logoRadios = document.querySelectorAll('input[name="logo"]');
 
 let isEnabled;
 
-let enabledButton = document.getElementById('enabled-toggle');
-let enabledImage = document.getElementById('enabled-status-icon');
-let enabledLabel = enabledButton.querySelector('span');
+const enabledButton = document.getElementById('enabled-toggle');
+const enabledImage = document.getElementById('enabled-status-icon');
+const enabledLabel = enabledButton.querySelector('h4');
 
-// Load the state from storage when the popup is opened
-chrome.storage.sync.get(['enabled'], function(result) {
+chrome.storage.sync.get(['enabled'], function (result) {
     isEnabled = result.hasOwnProperty('enabled') ? result.enabled : true;
     enabledButton.classList.toggle('enabled', isEnabled);
-    enabledImage.src = isEnabled ? "images/enabled.png" : "images/disabled.png";
-    enabledLabel.textContent = isEnabled ? "Enabled" : "Disabled";
+    enabledImage.src = isEnabled ? 'images/enabled.png' : 'images/disabled.png';
+    enabledLabel.textContent = isEnabled ? 'Enabled' : 'Disabled';
 });
 
-// Add event listener to toggle enabled status and image
-enabledButton.addEventListener('click', function() {
+enabledButton.addEventListener('click', function () {
     isEnabled = !isEnabled;
     enabledButton.classList.toggle('enabled', isEnabled);
-    enabledImage.src = isEnabled ? "images/enabled.png" : "images/disabled.png";
-    enabledLabel.textContent = isEnabled ? "Enabled" : "Disabled";
-    chrome.storage.sync.set({enabled: isEnabled});
+    enabledImage.src = isEnabled ? 'images/enabled.png' : 'images/disabled.png';
+    enabledLabel.textContent = isEnabled ? 'Enabled' : 'Disabled';
+    chrome.storage.sync.set({ enabled: isEnabled });
+});
+
+chrome.storage.sync.get(['selectedLogo'], function (result) {
+    let selectedLogo = result.hasOwnProperty('selectedLogo') ? result.selectedLogo : 'twitter';
+    document.querySelector(`input[value="${selectedLogo}"]`).checked = true;
+    disableAllLogoRadios();
+    document
+        .querySelector(`input[value="${selectedLogo}"]`)
+        .parentElement.classList.toggle('enabled', true);
+});
+
+function disableAllLogoRadios() {
+    logoRadios.forEach(function (radio) {
+        radio.parentElement.classList.toggle('enabled', false);
+    });
+}
+
+logoRadios.forEach(function (radio) {
+    radio.addEventListener('change', function () {
+        if (this.checked) {
+            chrome.storage.sync.set({ selectedLogo: this.value });
+            disableAllLogoRadios();
+            this.parentElement.classList.toggle('enabled', true);
+        }
+    });
 });
